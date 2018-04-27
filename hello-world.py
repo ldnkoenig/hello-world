c4327464f5c8ab4879dcac2fc6fd5d327fa30f86
Vincent King <ldnkoenig@gmail.com>
2018-04-27 21:41:11 +0100

    master

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,10 @@
-print ("Hello World")
+sAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = -3
+sMessage = input('Enter message: ')
+sNewMessage = ''
+for sCharacter in sMessage:
+	position = sAlphabet.find(sCharacter)
+	newPosition = (position + key)%26
+	sNewMessage += sAlphabet[newPosition]
+
+print (sNewMessage)
